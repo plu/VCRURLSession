fb1dc33e85b9ebe20d31d7a351a1047b132beb23
Johannes Plunien <plu@pqpq.de>
2016-01-06 09:07:26 +0400

    Init

---
+++ VCRURLSession/VCRURLSession.h
@@ -0,0 +1,13 @@
+//
+//  VCRURLSession.h
+//  VCRURLSession
+//
+//  Created by Plunien, Johannes on 06/01/16.
+//  Copyright © 2016 Johannes Plunien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VCRURLSession : NSObject
+
+@end
---
+++ VCRURLSession/VCRURLSession.m
@@ -0,0 +1,13 @@
+//
+//  VCRURLSession.m
+//  VCRURLSession
+//
+//  Created by Plunien, Johannes on 06/01/16.
+//  Copyright © 2016 Johannes Plunien. All rights reserved.
+//
+
+#import "VCRURLSession.h"
+
+@implementation VCRURLSession
+
+@end
